76520943d522e9d4d507eed6b7e47ea148251f9a
Saketh Thota <saketht@umich.edu>
2024-01-30 11:10:45 -0600

    updated font sizes for smaller devices

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
