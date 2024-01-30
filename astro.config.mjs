e7b4cd37d1ece1aefecce18bead20a651b805710
Saketh Thota <saketht@umich.edu>
2024-01-30 13:40:20 -0600

    some progress on post, also tons of config additions

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,21 +6,18 @@ import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import icon from "astro-icon";
 
+import expressiveCode from "astro-expressive-code";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-    mdx(),
-    icon(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), react(), icon(), expressiveCode({
+      theme: "rose-pine-moon",
+      styleOverrides: {}
+    }), mdx()],
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
-    shikiConfig: {
-      theme: "rose-pine-moon",
-    },
-  },
+  }
 });
