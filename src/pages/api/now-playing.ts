7d541a509ad3cb59ba912be201901defdac4fc5f
Saketh Thota <saketht@umich.edu>
2024-02-01 12:55:46 -0600

    fixed build issues

--- src/pages/api/now-playing.ts
+++ src/pages/api/now-playing.ts
@@ -1,6 +1,6 @@
 import { getNowPlaying } from '@/lib/spotify';
 
-export default async (_, res) => {
+export default async () => {
   const response = await getNowPlaying();
 
   if (response.status === 204 || response.status > 400) {
@@ -25,7 +25,7 @@ export default async (_, res) => {
 
   const isPlaying = song.is_playing;
   const title = song.item.name;
-  const artist = song.item.artists.map((_artist) => _artist.name).join(', ');
+  const artist = song.item.artists.map((_artist: any) => _artist.name).join(', ');
   const album = song.item.album.name;
   const albumImageUrl = song.item.album.images[0].url;
   const songUrl = song.item.external_urls.spotify;
