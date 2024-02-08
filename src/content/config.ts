1d739b7614f9b51b71be7ddc49f7ab4b9234bd96
Saketh Thota <saketht@umich.edu>
2024-02-08 09:41:44 -0600

    able to track blog view counts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,19 @@
-import { z, defineCollection } from 'astro:content';
+import { z, defineCollection } from "astro:content";
 
 const blog = defineCollection({
-  type: 'content',
+  type: "content",
   schema: z.object({
+    idx: z.number(),
     title: z.string(),
     description: z.string(),
     publishedAt: z.string(),
     tags: z.array(z.string()),
     draft: z.boolean(),
   }),
-})
+});
 
 const projects = defineCollection({
-  type: 'data',
+  type: "data",
   schema: z.object({
     title: z.string(),
     description: z.string(),
@@ -21,8 +22,6 @@ const projects = defineCollection({
     src: z.string(),
     tags: z.array(z.string()),
   }),
-})
+});
 
-export const collections = { blog, projects }
-
-
+export const collections = { blog, projects };
