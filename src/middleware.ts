ca96a343d716f6e364b7c2bab63af7202fa69ad3
Saketh Thota <sakeththota01@gmail.com>
2024-02-12 16:26:17 -0600

    created post file

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,10 @@
 import { defineMiddleware, sequence } from "astro:middleware";
 
 const updateViews = defineMiddleware(async ({ url, params }, next) => {
-  console.log("running");
+  /*
   if (import.meta.env.ASTRO_MODE === "build") {
     return next();
   }
-
   if (url.pathname.startsWith("/blog") && params.slug) {
     await fetch(url.origin + `/api/views/${params.slug}`, {
       method: "POST",
@@ -13,6 +12,7 @@ const updateViews = defineMiddleware(async ({ url, params }, next) => {
       body: JSON.stringify({}),
     });
   }
+  */
 
   return next();
 });
