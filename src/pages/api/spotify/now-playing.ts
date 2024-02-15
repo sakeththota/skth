e6c7b5eb41b6bbdcd85e678759d6d7d3a42d2eb7
Saketh Thota <sakeththota01@gmail.com>
2024-02-14 20:19:57 -0600

    modified post to an astro endpoint

--- src/pages/api/spotify/now-playing.ts
+++ src/pages/api/spotify/now-playing.ts
@@ -1,14 +1,15 @@
-import { getNowPlaying } from '@/lib/spotify';
+import type { APIRoute } from "astro";
+import { getNowPlaying } from "@/lib/spotify";
 
-export default async () => {
+export const GET: APIRoute = async () => {
   const response = await getNowPlaying();
 
   if (response.status === 204 || response.status > 400) {
     return new Response(JSON.stringify({ isPlaying: false }), {
       status: 200,
       headers: {
-        'content-type': 'application/json'
-      }
+        "content-type": "application/json",
+      },
     });
   }
 
@@ -18,14 +19,16 @@ export default async () => {
     return new Response(JSON.stringify({ isPlaying: false }), {
       status: 200,
       headers: {
-        'content-type': 'application/json'
-      }
+        "content-type": "application/json",
+      },
     });
   }
 
   const isPlaying = song.is_playing;
   const title = song.item.name;
-  const artist = song.item.artists.map((_artist: any) => _artist.name).join(', ');
+  const artist = song.item.artists
+    .map((_artist: any) => _artist.name)
+    .join(", ");
   const album = song.item.album.name;
   const albumImageUrl = song.item.album.images[0].url;
   const songUrl = song.item.external_urls.spotify;
@@ -37,14 +40,14 @@ export default async () => {
       artist,
       isPlaying,
       songUrl,
-      title
+      title,
     }),
     {
       status: 200,
       headers: {
-        'content-type': 'application/json',
-        'cache-control': 'public, s-maxage=60, stale-while-revalidate=30'
-      }
-    }
+        "content-type": "application/json",
+        "cache-control": "public, s-maxage=60, stale-while-revalidate=30",
+      },
+    },
   );
 };
