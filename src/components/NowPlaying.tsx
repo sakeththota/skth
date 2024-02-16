b8b573f74ec068b7651bbd4de2353cada817ec32
Saketh Thota <sakeththota01@gmail.com>
2024-02-15 19:53:42 -0600

    basic component working on index

---
+++ src/components/NowPlaying.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+
+export const NowPlaying = () => {
+  const [data, setData] = useState(null);
+
+  const fetchNowPlaying = async () => {
+    const res = await fetch("http://localhost:4321/api/spotify/now-playing");
+    return res.json();
+  };
+
+  useEffect(() => {
+    fetchNowPlaying().then((data) => {
+      console.log(data);
+      setData(data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h2>Now Playing</h2>
+      {data && (
+        <div>
+          <div>{data.title}</div>
+          <div>{data.artist}</div>
+          <img src={data.albumImageUrl} alt="album cover" />
+        </div>
+      )}
+    </div>
+  );
+};
