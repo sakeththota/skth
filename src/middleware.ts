9d976958f60d98f4ee5ea6ee9fb52701b50608da
Saketh Thota <sakeththota01@gmail.com>
2024-07-10 15:45:41 -0500

    quick fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,20 @@
-// import { defineMiddleware, sequence } from "astro:middleware";
+import { defineMiddleware, sequence } from "astro:middleware";
 
-// const updateViews = defineMiddleware(async ({ url, params }, next) => {
-//   console.log("running");
-//   if (import.meta.env.ASTRO_MODE === "build") {
-//     return next();
-//   }
+const updateViews = defineMiddleware(async ({ url, params }, next) => {
+  console.log("running");
+  // if (import.meta.env.ASTRO_MODE === "build") {
+  //   return next();
+  // }
 
-//   if (url.pathname.startsWith("/blog") && params.slug) {
-//     await fetch(url.origin + `/api/views/${params.slug}`, {
-//       method: "POST",
-//       headers: { "Content-Type": "application/json" },
-//       body: JSON.stringify({}),
-//     });
-//   }
+  // if (url.pathname.startsWith("/blog") && params.slug) {
+  //   await fetch(url.origin + `/api/views/${params.slug}`, {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify({}),
+  //   });
+  // }
 
-//   return next();
-// });
+  return next();
+});
 
-// export const onRequest = sequence(updateViews);
+export const onRequest = sequence(updateViews);
