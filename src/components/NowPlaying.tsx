487688fcd1c0829d4738c8dd9ccf16bd2af61689
Saketh Thota <sakeththota01@gmail.com>
2024-11-27 11:53:29 -0600

    added a tooltip to the spotify component and moved it to its own react component

--- src/components/NowPlaying.tsx
+++ src/components/NowPlaying.tsx
@@ -1,30 +1,33 @@
-import React, { useEffect, useState } from "react";
-
-export const NowPlaying = () => {
-  const [data, setData] = useState<any>(null);
-
-  const fetchNowPlaying = async () => {
-    const res = await fetch("http://localhost:4321/api/spotify/now-playing");
-    return res.json();
-  };
-
-  useEffect(() => {
-    fetchNowPlaying().then((data) => {
-      console.log(data);
-      setData(data);
-    });
-  }, []);
-
+import React from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+export const NowPlaying = ({ nowPlaying }) => {
   return (
-    <div>
-      <h2>Now Playing</h2>
-      {data && (
-        <div>
-          <div>{data.title}</div>
-          <div>{data.artist}</div>
-          <img src={data.albumImageUrl} alt="album cover" />
-        </div>
-      )}
-    </div>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger>
+          <div className="flex h-20 gap-4 rounded-md p-2 w-80">
+            <div className="relative aspect-square h-full w-auto">
+              <img
+                className="rounded-md absolute inset-0 h-full w-full object-cover"
+                src={nowPlaying.albumImageUrl}
+              />
+            </div>
+            <div className="flex flex-col justify-center items-start overflow-hidden whitespace-nowrap">
+              <p className="text-xs text-muted-foreground">Now Playing</p>
+              <h2 className="text-lg font-bold">{nowPlaying.title}</h2>
+              <p className="text-semibold -mt-1">{nowPlaying.artist}</p>
+            </div>
+          </div>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>This component tracks my Spotify activity live!</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 };
